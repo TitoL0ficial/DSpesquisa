65333917923d7f5da94648ca140e1eb8abb8b87e
Tito0ficial <titol.oficial@gmail.com>
2025-10-27 18:41:52 -0300

    Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import Logo from '../../assets/logo.svg'
+
+export default function Header () {
+
+    return (
+        <header className="main-header">
+            <img src={Logo} alt="Logo" />
+            <div className="logo-text">
+                <span className="logo-text-1">Big Game</span>
+                <span className="logo-text-2"> Survey</span>
+            </div>
+        </header>
+    );
+}
